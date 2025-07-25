f498bf3db95cae3dc5cecae51ee20fda4e7f653f
Ren-Mor <renatomorelli@hotmail.it>
2025-07-25 15:02:59 +0200

    fix value

--- src/main/java/Ren_Mor/gestione_viaggi/config/ConfigCloudinary.java
+++ src/main/java/Ren_Mor/gestione_viaggi/config/ConfigCloudinary.java
@@ -1,7 +1,7 @@
 package Ren_Mor.gestione_viaggi.config;
 
 import com.cloudinary.Cloudinary;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
